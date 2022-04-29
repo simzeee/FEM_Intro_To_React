5642c5d833403eece045f59164d5c77abc74c139
John Sims <jwsims816@gmail.com>
2022-04-28 21:39:53 -0500

    useState done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet"
+import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Eddie",
-      animal: "Dog",
-      breed: "Toy Poodle",
-    }),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Kitty",
-      animal: "Cat",
-      breed: "Street Cat",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+      <Pet name="Eddy" animal="Dog" breed="Toy Poodle"></Pet>
+      <Pet name="Kitty" animal="Cat" breed="Street Cat"></Pet>
+      <SearchParams/>
+    </div>
+  );
 };
+
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt me!"),
+//     React.createElement(Pet, {
+//       name: "Eddie",
+//       animal: "Dog",
+//       breed: "Toy Poodle",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Kitty",
+//       animal: "Cat",
+//       breed: "Street Cat",
+//     }),
+//   ]);
+// };
 
 render(React.createElement(App), document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  // const location = "Chicago, IL";
+
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["poodle", "something"];
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((allBreed) => (
+              <option key={allBreed} value={allBreed}>
+                {allBreed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
