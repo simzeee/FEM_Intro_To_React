f4f0d791a103883c72c53df05390bd8a23190835
John Sims <74082034+simzeee@users.noreply.github.com>
2022-04-28 19:27:22 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Eddie",
+      animal: "Dog",
+      breed: "Toy Poodle",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Kitty",
+      animal: "Cat",
+      breed: "Street Cat",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
